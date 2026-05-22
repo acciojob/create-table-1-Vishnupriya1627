b5a2ab652fdb2f125011590a2541d6d4fc6bff39
Vishnupriya1627 <mamillapallivishnupriya@gmail.com>
2026-05-22 16:59:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function insert_Row() {
-    //Write your code here
-  const table = document.querySelector('#sampleTable');
+
+  const table = document.querySelector('#sampleTable tbody');
+
   const newRow = document.createElement('tr');
+
   newRow.innerHTML = `
-	  <td>New Cell1</td>
-	  <td>New Cell2</td>
-	  `
-	table.prepend(newRow);
-  
+      <td>New Cell1</td>
+      <td>New Cell2</td>
+  `;
+
+  table.prepend(newRow);
+
 }
