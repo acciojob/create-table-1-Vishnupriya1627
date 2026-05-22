2e6627f1291bb85a341c9075cb40e47d20ca2bec
Vishnupriya1627 <mamillapallivishnupriya@gmail.com>
2026-05-22 16:44:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
-  
+  const table = document.querySelector('#sampleTable');
+  const newRow = document.createElement('tr');
+  newRow.innerHTML = `
+	  <td>New Cell1</td>
+	  <td>New Cell2</td>
+	  `
+	table.innerHTML = newRow.innerHTML+table.innerHTML;
+	body.appendChild(table);
   
 }
